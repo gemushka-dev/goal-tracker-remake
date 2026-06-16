59a1ff4c6464f0a7002eb4e0c187db7c99ac03df
unknown <iskandar.django.work@gmail.com>
2026-06-16 10:30:52 +0500

    feat(comments): add validation to comment dto

--- src/common/dto/comment.dto.ts
+++ src/common/dto/comment.dto.ts
@@ -1,11 +1,32 @@
-export type CommentGoalDTO = {
-  commentText: string;
-  userId: number;
-  goalId: number;
-};
-export type CommentCommentDTO = {
-  commentText: string;
-  userId: number;
-  goalId: number;
-  parentId: number;
-};
+import { IsInt, IsNotEmpty, IsPositive, IsString } from "class-validator";
+
+export class CommentGoalDTO {
+  @IsString()
+  @IsNotEmpty()
+  commentText!: string;
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  userId!: number;
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  goalId!: number;
+}
+export class CommentCommentDTO {
+  @IsString()
+  @IsNotEmpty()
+  commentText!: string;
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  userId!: number;
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  goalId!: number;
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  parentId!: number;
+}
