b501747b6ef1ad3d149745b0c9e6e2789225ad13
unknown <iskandar.django.work@gmail.com>
2026-06-18 10:21:05 +0500

    feat(likes): add validation for like dto

--- src/common/dto/like.dto.ts
+++ src/common/dto/like.dto.ts
@@ -1,8 +1,22 @@
-export type LikeGoalDTO = {
-  userId: number;
-  goalId: number;
-};
-export type LikeCommentDTO = {
-  userId: number;
-  commentId: number;
-};
+import { IsInt, IsNotEmpty, IsPositive } from "class-validator";
+
+export class LikeGoalDTO {
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  userId!: number;
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  goalId!: number;
+}
+export class LikeCommentDTO {
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  userId!: number;
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  commentId!: number;
+}
